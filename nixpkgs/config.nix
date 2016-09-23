c470dea2d6746942e28c268a566fe0fdcc705851
Cody W. Opel <codyopel@gmail.com>
2016-09-23 06:47:08 -0400

    nixpkgs: split packages into multiple envs

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -9,19 +9,19 @@ pkgs: {
 
     chromium = self.chromium.override {
       proprietaryCodecs = true;
-      enableWideVine = true;
-      enablePepperFlash = true;
+      #enableWideVine = true;
+      #enablePepperFlash = true;
     };
     #emacs = self.emacs.override {
     #  withX = false;
     #};
     pulseaudio_full = self.pulseaudio_full.override {
       loopbackLatencyMsec = "20";
-      resampleMethod = "speex-float-7";
+      resampleMethod = "speex-float-5";
     };
-    rtorrent = self.rtorrent.override {
-      colorSupport = true;
-    };
+    #rtorrent = self.rtorrent.override {
+      #  colorSupport = true;
+      #};
     #x264 = self.x264.override {
     #  enable10bit = true;
     #};
@@ -31,157 +31,214 @@ pkgs: {
       #yi
     #]);
 
-    # Import Environments
-    user-env = self.buildEnv {
+    /*user-env = self.buildEnv {
       name = "userEnv";
       paths = with self; [
-        # Default
-          acpi
-          atop
-          bc
-          brotli
-          curl
-          dash
-          dmidecode
-          dnstop
-          git
-          gptfdisk
-          #hdparm
-          htop
-          iftop
-          iotop
-          iperf
-          ipset
-          iptables
-          jq
-          lib-bash
-          lm-sensors
-          meslo-lg
-          mg
-          mtr
-          nftables
-          nmap
-          openssh
-          openssl
-          p7zip
-          psmisc
-          screen
-          smartmontools
-          speedtest-cli
-          sysstat
-          tcpdump
-          tmux
-          unrar
-          unzip
-          vim
-          wget
-          xz
-          zsh
 
         # Headless
-          #goPackages.hugo.bin
-          #goPackages.ipfs.bin
-          goPackages.lego.bin
-          goPackages.nomad.bin
-          #goPackages.syncthing.bin
-          goPackages.vault.bin
-          python2Packages.bazaar
-          python3Packages.beets
-          #python2Packages.certbot
-          python2Packages.deluge
-          python3Packages.flexget
-          python3Packages.pycountry
-          #python2Packages.youtube-dl
-          python3Packages.python
-          python3Packages.youtube-dl
-          arkive
-          atom
-          btsync
-          cdrtools
-          ffmpeg_head
-          flac
-          gnupg
-          pinentry
-          go
-          #icedtea8_web
-          imagemagick
-          lame
-          #libpng
-          #libvpx
-          mediainfo
-          mosh
-          mpd
-          #mpdris2
-          ncdc
-          #ncdu
-          ncmpcpp
-          networkmanager
-          #nix-repl
-          #nixops
+          #mosh
           #notbit
-          pcsc-lite_full
-          perl
-          pinentry
-          psmisc
-          pulseaudio_full
-          rtorrent
-          #rustc
-          scrot
-          slock
-          subversion
-          #vobsub2srt
-          x264
-          x265
+          #pinentry
+          #psmisc
           #xlibs.xbacklight
 
         # Graphical
-          chromium
-          dmenu
           #eagle
-          eog
-          file-roller
-          firefox
-          #gimp
-          #gnome-calculator
-          gnome-mpv
-          gnome-screenshot
-          gnome-terminal
           #gnome-tweak-tool
-          #guitarix
-          #jack2_full
           #kde5.kate
-          lftp
-          #libreoffice
-          #mixxx
-          #mkvtoolnix
-          mpv
-          mumble
-          nautilus
-          networkmanager
-          networkmanager-applet
-          nvidia-settings
-          pavucontrol
           #kde5.quasselClient
-          #qjackctl
-          sakura
-          split2flac
-          sublime-text
-          swig
-          #teamspeak_client
-          #texLive
-          #texstudio
-          ufraw
           #virtmanager
-          #vlc
-          #xfe
+      ];
+    };*/
 
-          #myHsEnv
+    /*
+    name-env = self.buildEnv {
+      name = "nameEnv";
+      paths = with self; [
 
-          #steamEnv
+      ];
+    };
+    */
+
+    default-tools-env = self.buildEnv  {
+      name = "defaultToolsEnv";
+      paths = with self; [
+        acpi
+        atop
+        bash
+        bc
+        brotli
+        curl
+        dash
+        dmidecode
+        dnstop
+        git
+        gptfdisk
+        #hdparm
+        htop
+        iftop
+        iotop
+        iperf
+        ipset
+        iptables
+        jq
+        lib-bash
+        lm-sensors
+        mg
+        mtr
+        nftables
+        nmap
+        openssh
+        openssl
+        p7zip
+        psmisc
+        screen
+        smartmontools
+        speedtest-cli
+        subversion
+        sysstat
+        tcpdump
+        tmux
+        unrar
+        unzip
+        vim
+        wget
+        xz
+        zsh
+
+        gnupg
+        pcsc-lite_full
       ];
     };
 
-    myHsEnv = self.haskellPackages.ghcWithPackages (self : with self; [
+    desktop-env = self.buildEnv {
+      name = "desktopEnv";
+      paths = with self; [
+        dmenu
+        eog
+        file-roller
+        #gnome-calculator
+        gnome-screenshot
+        gnome-terminal
+        mumble
+        nautilus
+        networkmanager
+        networkmanager-applet
+        nvidia-settings
+        #pavucontrol
+        pulseaudio_full
+        sakura
+        scrot
+        slock
+        #teamspeak_client
+        #xfe
+      ];
+    };
+
+    development-env = self.buildEnv {
+      name = "developmentEnv";
+      paths = with self; [
+        go
+        #goPackages.hugo.bin
+        #goPackages.ipfs.bin
+        goPackages.lego.bin
+        goPackages.mc.bin
+        #goPackages.minio.bin
+        goPackages.nomad.bin
+        goPackages.vault.bin
+        #icedtea8_web
+        openjdk
+        perl
+        python3Packages.aws-cli
+        python2Packages.bazaar
+        #python2Packages.certbot
+        python3Packages.python
+        #rustc
+        swig
+        #texLive
+        #texstudio
+      ];
+    };
+
+    multimedia-env = self.buildEnv {
+      name = "multimediaEnv";
+      paths = with self; [
+        gnome-mpv
+        mpd
+        #mpdris2
+        mpv
+        ncmpcpp
+        #python2Packages.mopidy
+        #vlc
+      ];
+    };
+
+    multimedia-tools-env = self.buildEnv {
+      name = "multimediaToolsEnv";
+      paths = with self; [
+        arkive
+        imagemagick
+        #gimp
+        #guitarix
+        #jack2_full
+        lame
+        #libpng
+        libvpx_head
+        python3Packages.beets
+        python3Packages.pycountry
+        cdrtools
+        ffmpeg_head
+        flac
+        mediainfo
+        #mixxx
+        #mkvtoolnix
+        split2flac
+        #qjackctl
+        ufraw
+        #vobsub2srt
+        x264
+        x265
+      ];
+    };
+
+    editor-env = self.buildEnv {
+      name = "editorEnv";
+      paths = with self; [
+        atom
+        #libreoffice
+        sublime-text
+      ];
+    };
+
+    p2p-env = self.buildEnv {
+      name = "p2pEnv";
+      paths = with self; [
+        btsync
+        #goPackages.syncthing.bin
+        lftp
+        ncdc
+        python2Packages.deluge
+        python3Packages.flexget
+        python3Packages.youtube-dl
+        rtorrent
+      ];
+    };
+
+    chromium-env = self.buildEnv  {
+      name = "chromiumEnv";
+      paths = with self; [
+        chromium
+      ];
+    };
+
+    firefox-env = self.buildEnv  {
+      name = "firefoxEnv";
+      paths = with self; [
+        firefox
+      ];
+    };
+
+    myHsEnv = self.haskellPackages.ghcWithPackages (self: with self; [
       #pandoc
       xmonad
       xmonad-contrib
