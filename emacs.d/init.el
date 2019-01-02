af4ae16ae9d546d4c5ded31475a2516a1903fa8f
Cody W. Opel <codyopel@gmail.com>
2019-01-01 21:10:17 -0500

    drop emacs configs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,26 +6,13 @@
 
 (set-face-attribute 'default nil :height 90)
 
-; ---------------------------
-;     Syntax Highlighting
-; ---------------------------
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(load-theme 'monokai t)
+;;; Syntax Highlighting
+;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+;(load-theme 'monokai t)
 
-; ---------------------------
-;        Line Numbers
-; ---------------------------
+;;; Line Numbers
 (global-linum-mode 1)
 ; Pad space around numbers
 (setq linum-format " %d ")
-
 (column-number-mode 1)
 
-; ---------------------------
-;          Mini Map
-; ---------------------------
-(add-to-list 'load-path "~/.emacs.d/plugins/sublimity/")
-(require 'sublimity)
-(require 'sublimity-scroll)
-(require 'sublimity-map)
-(sublimity-mode 1)
