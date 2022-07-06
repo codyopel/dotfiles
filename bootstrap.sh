eac0bbf44e55dc1b677d2fddcac3e1e80d97709a
Cody W. Opel <cwopel@chlorm.net>
2022-07-06 08:37:28 -0500

    Add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+git submodule init
+git submodule update
+
+mkdir -p $HOME/.config/elvish/
+ln -sf $HOME/.dotfiles/config/elvish/rc.elv $HOME/.config/elvish/rc.elv
+
+elvish -c '
+use epm
+epm:install github.com/chlorm/elvish-dotfile-manager
+
+use github.com/chlorm/elvish-dotfile-manager/dotfiles
+dotfiles:install (get-env HOME)/.dotfiles/
+'
+
+exec elvish
+exit $?
+
