3d182b6c48125d99346f31abdddd1e8accf9c6e2
Cody W. Opel <cwopel@chlorm.net>
2022-07-25 12:48:28 -0500

    neovim: Automatically close nvim-tree

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -381,3 +381,14 @@ vim.api.nvim_create_autocmd('BufWritePost', {
     group = packer_group,
     command = 'source <afile> | PackerCompile'
 })
+
+-- When quitting automatically close nvim-tree window.
+vim.api.nvim_create_autocmd('BufEnter', {
+    nested = true,
+    callback = function()
+        if #vim.api.nvim_list_wins() == 1
+                and vim.api.nvim_buf_get_name(0):match('NvimTree_') ~= nil then
+            vim.cmd('quit')
+        end
+    end
+})
