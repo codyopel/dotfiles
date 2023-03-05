e3e354ebd2a356aa892502ce6eb8cf41091eb1af
Cody W. Opel <cwopel@chlorm.net>
2023-03-05 01:06:09 -0600

    neovim: Add neoscroll plugin

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -721,6 +721,20 @@ use { 'petertriho/nvim-scrollbar',
         })
     end,
 }
+--use { 'karb94/neoscroll.nvim',
+--    config = function()
+--        vim.keymap.set('n', '<ScrollWheelUp>', '<C-y>')
+--        vim.keymap.set('n', '<ScrollWheelDown>', '<C-e>')
+--        vim.keymap.set('i', '<ScrollWheelUp>', '<C-y>')
+--        vim.keymap.set('i', '<ScrollWheelDown>', '<C-e>')
+--        vim.keymap.set('v', '<ScrollWheelUp>', '<C-y>')
+--        vim.keymap.set('v', '<ScrollWheelDown>', '<C-e>')
+--
+--        require('neoscroll').setup({
+--            mappings = { '<C-y>', '<C-e>' },
+--        })
+--    end,
+--}
 -- Indent guides
 use { 'lukas-reineke/indent-blankline.nvim',
     event = 'VimEnter',
